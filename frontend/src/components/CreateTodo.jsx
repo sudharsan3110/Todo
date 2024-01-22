972cf88f1449b58087ed5970cbf37f822c34eff2
SUDHAARSAN <115058137+sudharsan3110@users.noreply.github.com>
2024-01-22 20:32:28 +0530

    files updated

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,20 @@
+export function CreateTodo(){
+    return (
+    <div>
+        <input style= {{
+            padding : 10,
+            margin : 10
+        }} type="text" placeholder="text"></input>
+         <br></br>
+        <input style= {{
+            padding : 10,
+            margin : 10
+        }}type="text" placeholder="description"></input>
+        <br></br>
+        <button style= {{
+            padding : 10,
+            margin : 10
+        }}>Add the task </button>
+    </div>
+)}
+
---
+++ frontend/src/components/Todo.jsx
@@ -0,0 +1,12 @@
+export function Todo({todos}){
+    
+    return <div>
+        { todos.map(function(todo){
+                return <div>
+                    <h1>{todo.title}</h1>
+                    <h1>{todo.description}</h1>
+                    <button>{todo.completed == true ? "completed" : "mark as completed"}</button>
+                </div>
+            })}
+     </div>
+}
